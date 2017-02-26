53f69425c4730ee6006682050436a5bff0daedaf
Ankur Bag <bag.a@husky.neu.edu>
2017-02-26 17:50:42 -0500

    Alpha:Testing listen

--- server.js
+++ server.js
@@ -10,10 +10,13 @@ app.use(express.static(__dirname + '/public'));
 
 require ("./test/app.js")(app);
 
-var port = process.env.PORT || 3000;
+//var port = process.env.PORT || 3000;
 
 //app.listen(port);
-
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
+/*
 server.listen(port, function() {
     console.log("App is running on port " + port);
-});
+});*/
